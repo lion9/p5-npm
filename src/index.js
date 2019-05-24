bd1dd6fbeff93b6abc691f66df9356992e79351b
Artyom <a.vecherov@gmail.com>
2019-05-24 13:45:53 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import p5 from 'p5';
+
+function setup() {
+    createCanvas(10, 10);
+}
+
+function draw() {
+    background(0);
+    circle(10, 10, 10);
+}
+
+console.log('text');
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'app.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
